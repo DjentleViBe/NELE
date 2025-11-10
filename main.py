10df7b26ee9748f9e018c008c4c64e596a4117ce
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-10 08:27:33 +0100

    gelu

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 from non_linear_tanh import tanh_net
 from non_linear_relu import relu_net
 from non_linear_elu import elu_net
+from non_linear_gelu import gelu_net
 from plot_pred import plot_pred
 from plot_loss import plot_loss
 
@@ -17,16 +18,23 @@ if __name__ == "__main__":
     print("Running analysis - ELU Activation Function")
     elu_net(x, y)
 
+    print("Running analysis - GELU Activation Function")
+    gelu_net(x, y)
+
     print("Plotting results")
     plot_pred(['RESULTS/predictions_tanh.csv',
                'RESULTS/predictions_relu.csv',
-               'RESULTS/predictions_elu.csv'], 
+               'RESULTS/predictions_elu.csv',
+               'RESULTS/predictions_gelu.csv'], 
                ['Tanh',
                 'ReLU',
-                'ELU'])
+                'ELU',
+                'GELU'])
     plot_loss(['RESULTS/loss_history_tanh.csv',
                'RESULTS/loss_history_relu.csv',
-               'RESULTS/loss_history_elu.csv'], 
+               'RESULTS/loss_history_elu.csv',
+               'RESULTS/loss_history_gelu.csv'], 
               ['Tanh',
                 'ReLU',
-                'ELU'])
+                'ELU',
+                'GELU'])
