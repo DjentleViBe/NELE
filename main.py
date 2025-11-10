fb3695ad714616b474df330831c8b0ee195796e2
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-10 08:32:30 +0100

    signoid

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from non_linear_tanh import tanh_net
 from non_linear_relu import relu_net
 from non_linear_elu import elu_net
 from non_linear_gelu import gelu_net
+from non_linear_sigmoid import sigmoid_net
 from plot_pred import plot_pred
 from plot_loss import plot_loss
 
@@ -21,20 +22,27 @@ if __name__ == "__main__":
     print("Running analysis - GELU Activation Function")
     gelu_net(x, y)
 
+    print("Running analysis - Signoid Activation Function")
+    sigmoid_net(x, y)
+
     print("Plotting results")
     plot_pred(['RESULTS/predictions_tanh.csv',
                'RESULTS/predictions_relu.csv',
                'RESULTS/predictions_elu.csv',
-               'RESULTS/predictions_gelu.csv'], 
+               'RESULTS/predictions_gelu.csv',
+               'RESULTS/predictions_sigmoid.csv'], 
                ['Tanh',
                 'ReLU',
                 'ELU',
-                'GELU'])
+                'GELU',
+                'Sigmoid'])
     plot_loss(['RESULTS/loss_history_tanh.csv',
                'RESULTS/loss_history_relu.csv',
                'RESULTS/loss_history_elu.csv',
-               'RESULTS/loss_history_gelu.csv'], 
+               'RESULTS/loss_history_gelu.csv',
+               'RESULTS/loss_history_sigmoid.csv'], 
               ['Tanh',
                 'ReLU',
                 'ELU',
-                'GELU'])
+                'GELU',
+                'Sigmoid'])
