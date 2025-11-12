5ce036052b327d703b10463b62f2b5e2c73ef6d0
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-12 06:42:25 +0100

    stuy lists

--- main.py
+++ main.py
@@ -3,20 +3,43 @@ from study import study_data
 from file_operations import reset_directory
 
 if __name__ == "__main__":
+    
     reset_directory('./RESULTS/')
     reset_directory('./PICS/')
-
+    
     ################### SINE NOISE ##########################
     reset_directory('./PICS/sine_noise')
     reset_directory('./RESULTS/sine_noise')
     x = torch.linspace(-3, 3, 200).unsqueeze(1)
     y = torch.sin(x) + 0.2 * torch.randn(x.size())
-    study_data(x, y, study_type='sine_noise')
+    study_data(x, y, 500, study_type='sine_noise')
 
     ################### TRIG NOISE ##########################
     reset_directory('./PICS/trig_noise')
     reset_directory('./RESULTS/trig_noise')
     x = torch.linspace(-3, 3, 200).unsqueeze(1)
     y = torch.sin(x) + 0.5 * torch.sin(3*x) + 0.2 * torch.randn(x.size())
-    study_data(x, y, study_type='trig_noise')
+    study_data(x, y, 500, study_type='trig_noise')
+    
+    ################### EXP NOISE ##########################
+    reset_directory('./PICS/exp_noise')
+    reset_directory('./RESULTS/exp_noise')
+    x = torch.linspace(-3, 3, 200).unsqueeze(1)
+    y = torch.exp(-0.5*x) + 0.2 * torch.randn(x.size())
+    study_data(x, y, 500, study_type='exp_noise')
+    
+    ################### HYP NOISE ##########################
+    reset_directory('./PICS/hyp_noise')
+    reset_directory('./RESULTS/hyp_noise')
+    x = torch.linspace(-3, 3, 200).unsqueeze(1)
+    y = torch.tanh(x) + 0.2 * torch.randn(x.size())
+    study_data(x, y, 500, study_type='hyp_noise')
+    
+    ################### QUAD NOISE ##########################
+    reset_directory('./PICS/quad_noise')
+    reset_directory('./RESULTS/quad_noise')
+    x = torch.linspace(-3, 3, 200).unsqueeze(1)
+    y = torch.tensor(x**2) + 0.2 * torch.randn(x.size())
+    study_data(x, y, 500, study_type='quad_noise')
+
     
--- plot_pred.py
+++ plot_pred.py
@@ -2,14 +2,12 @@ import matplotlib.pyplot as plt
 import torch
 from csv_operations import csv_read
 
-def plot_pred(predfiles, labels, study_type='default'):
+def plot_pred(x, y, predfiles, labels, study_type='default'):
     colors = ['#1f77b4', '#ff7f0e', '#2ca02c', 
               '#d62728', '#9467bd', '#8c564b', 
               '#e377c2', '#7f7f7f', '#000000',
               '#bcbd22', 
               '#17becf']
-    x = torch.linspace(-3, 3, 200).unsqueeze(1)
-    y = torch.sin(x) + 0.2 * torch.randn(x.size())
     plt.figure(figsize=(8,5))
     # Read the CSV file
     for predfile, label, color in zip(predfiles, labels, colors):
