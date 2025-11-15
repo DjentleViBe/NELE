78a0241b579f838afb28e481922aae41c1202221
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-15 15:19:34 +0100

    csv

--- csv_operations.py
+++ csv_operations.py
@@ -1,19 +1,21 @@
 import csv 
 
-def csv_write(filename, x, predicted, xname, yname):
+def csv_write(filename, x, predicted, xname, yname, zname, y_actual):
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([xname, yname])  # header
-        for xi, yi in zip(x, predicted):
-            writer.writerow([xi.item(), yi.item()])  # convert tensors to Python floats
+        writer.writerow([xname, yname, zname])  # header
+        for xi, yi, zi in zip(x, predicted, y_actual):
+            writer.writerow([xi.item(), yi.item(), zi.item()])  # convert tensors to Python floats
     
-def csv_read(filename, xname, yname):
+def csv_read(filename, xname, yname, zname):
     x_vals = []
     y_preds = []
+    z_preds = []
     reader = csv.DictReader(filename)  # read as dictionary
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             x_vals.append(float(row[xname]))
             y_preds.append(float(row[yname]))
-    return x_vals, y_preds
+            z_preds.append(float(row[zname]))
+    return x_vals, y_preds, z_preds
