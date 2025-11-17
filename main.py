d1bd132f1cb4dbbf9d77ae81923ab27b07decd52
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-17 14:05:35 +0100

    lr

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from study import study_data
 from file_operations import reset_directory
 
 if __name__ == "__main__":
-    
+    '''
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
     reset_directory('./RESULTS/')
     reset_directory('./PICS/')
@@ -20,14 +20,14 @@ if __name__ == "__main__":
     reset_directory('./RESULTS/trig_noise')
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
     y = torch.sin(x) + 0.5 * torch.sin(3*x) + 0.2 * torch.randn(x.size())
-    study_data(x, y, 4000, 0.0005, study_type='trig_noise')
+    study_data(x, y, 10000, 0.0001, study_type='trig_noise')
     
     ################### EXP NOISE ##########################
     reset_directory('./PICS/exp_noise')
     reset_directory('./RESULTS/exp_noise')
     
     y = torch.exp(-0.5*x) + 0.2 * torch.randn(x.size())
-    study_data(x, y, 4000, 0.0005, study_type='exp_noise')
+    study_data(x, y, 10000, 0.0001, study_type='exp_noise')
     
     ################### HYP NOISE ##########################
     reset_directory('./PICS/hyp_noise')
@@ -50,12 +50,12 @@ if __name__ == "__main__":
     y = torch.tensor(x**3) / (torch.exp(x) - 1 + 1E-6) + 0.2 * torch.randn(x.size())
     study_data(x, y, 2000, 0.001, study_type='exppoly_noise')
     '''
-
-    ################### TRIG NOISE ##########################
-    reset_directory('./PICS/trig_noise')
-    reset_directory('./RESULTS/trig_noise')
+    ################### HYP NOISE ##########################
+    reset_directory('./PICS/hyp_noise')
+    reset_directory('./RESULTS/hyp_noise')
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
-    y = torch.sin(x) + 0.5 * torch.sin(3*x) + 0.2 * torch.randn(x.size())
-    study_data(x, y, 4000, 0.0005, study_type='trig_noise')
-    '''
+    y = torch.tanh(x) + 0.2 * torch.randn(x.size())
+    study_data(x, y, 3000, 0.0005, study_type='hyp_noise')
+
+    
     
