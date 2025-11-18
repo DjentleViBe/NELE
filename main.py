5ba8292d893a08d3d459e42044c65267ab8faca3
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-18 17:49:27 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from curve_fit import curve_fit
-from mnist import mnist
+from mnist import mnist, mnist_validation
 
 if __name__ == "__main__":
+    print("Begin Analysis")
     # Synthetic data analysis
     # curve_fit()
     # MNIST
     mnist()
     # CIFAR-10
+    print("Analysis completed")
     
