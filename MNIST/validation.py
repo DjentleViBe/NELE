52e7bf191cfb99a6b5deadfeffdba45a7d21bf8f
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-18 19:37:30 +0100

    validation load

--- MNIST/validation.py
+++ MNIST/validation.py
@@ -41,7 +41,10 @@ def mnist_validation(epochs, activation_type='default'):
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     model = DeepFCNet(input_size, hidden_size, num_hidden_layers, num_classes, activation, activation_type)
-    model.load_state_dict(torch.load('RESULTS/MNIST/' + activation_type + '/' + activation_type + '_' + str(epochs - 1) + '.pth'))
+    checkpoint = torch.load('RESULTS/MNIST/' + activation_type + '/' + activation_type + '_' + str(epochs - 1) + '.pth')
+    model.load_state_dict(checkpoint['model_state_dict'])
+    epoch = checkpoint['epoch']
+    epoch_loss = checkpoint['epoch_loss']
     model.eval()
     correct = 0
     total = 0
@@ -52,4 +55,4 @@ def mnist_validation(epochs, activation_type='default'):
             _, predicted = torch.max(outputs.data, 1)
             total += target.size(0)
             correct += (predicted == target).sum().item()
-    print(f'{activation_type}, Test Accuracy: {100 * correct / total:.2f}%')
+    print(f'{activation_type}, Test Accuracy: {100 * correct / total:.2f}%, Training loss : {epoch_loss}')
