d75e580b2848910261d2decfd9676005d1415a0a
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-19 09:25:18 +0100

    device arg

--- mnist.py
+++ mnist.py
@@ -2,37 +2,28 @@ from MNIST.study import mnist_data
 from file_operations import reset_directory
 from MNIST.validation import mnist_validation
 
-def mnist():
+def mnist(device):
     print("Starting MNIST")
     #reset_directory('./RESULTS/MNIST')
     #reset_directory('./PICS/MNIST')
-    '''
-    mnist_data(50, 0.01, 'elu')
+    mnist_data(20, 0.01, device, 'elu')
+    mnist_data(20, 0.01, device, 'gelu')
+    mnist_data(20, 0.01, device, 'relu')
+    mnist_data(20, 0.01, device, 'leaky_relu')
+    mnist_data(20, 0.01, device, 'sigmoid')
+    mnist_data(20, 0.01, device, 'softplus')
+    mnist_data(20, 0.01, device, 'tanh')
+    mnist_data(20, 0.01, device, 'mish')
+    mnist_data(20, 0.01, device, 'silu')
+    mnist_data(20, 0.01, device, 'lelu')
+    # mnist_data(20, 0.001, 'nele')
     
-    mnist_data(50, 0.01, 'gelu')
-    mnist_data(50, 0.01, 'relu')
-    mnist_data(50, 0.01, 'leaky_relu')
-    mnist_data(50, 0.01, 'sigmoid')
-    mnist_data(50, 0.01, 'softplus')
-    mnist_data(50, 0.01, 'tanh')
-    mnist_data(50, 0.01, 'mish')
-    mnist_data(50, 0.01, 'silu')
-    mnist_data(50, 0.01, 'lelu')
-    mnist_data(50, 0.001, 'nele')
-    
-    mnist_validation(50,'tanh')
-    mnist_validation(50, 'softplus')
-    mnist_validation( 50,'sigmoid')
-    mnist_validation( 50,'elu')
-    mnist_validation( 50,'silu')
-    mnist_validation( 50,'gelu')
-    mnist_validation( 50,'relu')
-    mnist_validation( 50,'leaky_relu')
-    mnist_validation( 50,'mish')
-    
-    mnist_validation( 50,'lelu')
-    mnist_validation(30, 'nele')
-    '''
-    mnist_data(100, 0.01, 'nele')
-    #mnist_data(100, 0.01, 'lelu')
-    #mnist_validation(100,'lelu')
+    mnist_validation(20,'tanh')
+    mnist_validation(20, 'softplus')
+    mnist_validation( 20,'sigmoid')
+    mnist_validation( 20,'elu')
+    mnist_validation( 20,'silu')
+    mnist_validation( 20,'gelu')
+    mnist_validation( 20,'relu')
+    mnist_validation( 20,'leaky_relu')
+    mnist_validation( 20,'mish')
