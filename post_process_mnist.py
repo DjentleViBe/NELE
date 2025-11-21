478cabcfa0f6a74d1b92de088c362df80364218f
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-21 11:10:42 +0100

    pot processs mnist

--- post_process_mnist.py
+++ post_process_mnist.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 activations =  ['Tanh', 'Sigmoid', 'Softplus', 'ELU', 'SiLU', 'GELU', 'ReLU', 'Leaky ReLU', 'LeLU', 'Mish', 'NELE']
-activations_file =  ['tanh', 'sigmoid', 'softplus', 'elu', 'silu', 'gelu', 'relu', 'leaky_relu', 'lelu', 'mish', 'nele']
+activations_file =  ['tanh', 'sigmoid', 'softplus', 'elu', 'silu', 'gelu', 'relu', 'leaky_relu', 'lelu', 'mish',  'nele']
 colors = ["#0000FF", "#aec7e8", 
             "#ff0000", "#ffbb78",
             "#2ca02c", "#98df8a",
@@ -13,28 +13,59 @@ colors = ["#0000FF", "#aec7e8",
             '#000000']
 
 def plot_only(study_type):
-    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize = (7,7))
+    fig.subplots_adjust(right = 0.7, hspace = 0.35) 
     # plt.bar(activations, std_deviation_collect, yerr=loss_collect, capsize=5, , ecolor="#c7c7c7", error_kw={"elinewidth": 2})
-    plt.ylabel('Loss')
-    plt.yscale('log')
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.savefig('./PICS/' + study_type + '/Loss_Bar_Chart_with_Error_Bars.pdf', transparent=False)
-    plt.cla()
-    plt.close()
-
-    plt.figure(figsize=(8,5))
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-
+    ax1.set_ylabel('Training Loss')
+    ax2.set_ylabel('Test Loss (%)')
+   
+    ax1.set_yscale('log')
+    ax1.set_xlabel('epochs')
+    ax3 = ax2.twinx()
+    ax4 = ax1.twiny()
+    ax5 = ax2.twiny()
+    ax3.set_ylabel('Test Loss (%)')
+    ax2.set_xlabel('epochs')
     for i, act in enumerate(activations_file):
-        epochs, losses_tanh, _ = csv_read('RESULTS/' + study_type  + '/' + act + '/loss_history_' + activations_file[i] + '.csv', 'epoch', 'loss', '')
-        plt.plot(epochs, losses_tanh, colors[i], label=activations[i], linewidth = 0.7)
-    # Plot
-    plt.yscale('log')
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
+        epochs, losses_train, losses_test = csv_read('RESULTS/' + study_type  + '/' + act + '/loss_history_' + activations_file[i] + '.csv', 'epoch', 'loss', '')
+        if act == 'nele' or act =='lelu':
+            ax4.plot(epochs, losses_train, color = colors[i], label=activations[i], linewidth = 0.7)
+        else:
+            ax1.plot(epochs, losses_train, colors[i], label=activations[i], linewidth = 0.7)
+        selected_epochs = []
+        selected_losses = []
+
+        for e, l in zip(epochs, losses_test):
+            if (e - 1) % 5 == 0:   # 1,6,11,...
+                selected_epochs.append(e)
+                selected_losses.append(l)
+        selected_epochs.append(epochs[-1])
+        selected_losses.append(losses_test[-1])
+        if act == 'sigmoid' or act == 'softplus':
+            ax3.plot(selected_epochs, selected_losses, color = colors[i], label=activations[i], linewidth = 0.7)
+        elif act == 'nele' or act =='lelu':
+            ax5.plot(selected_epochs, selected_losses, color = colors[i], label=activations[i], linewidth = 0.7)
+        else:
+            ax2.plot(selected_epochs, selected_losses, color = colors[i], label=activations[i], linewidth = 0.7)
+    
+    ax1.set_xlim(1, 20)
+    ax2.set_xlim(1, 20)
+    ax3.set_xlim(1, 20)
+    ax4.set_xlim(1, int(max(epochs)))
+    ax5.set_xlim(1, int(max(epochs)))
+    ax2.set_xticks(range(1, 20, 5))
+    ax1.grid(True, linewidth = 0.1)
+    ax2.grid(True, linewidth = 0.1)
+    lines = []
+    labels = []
+
+    for ax in [ax1, ax4]:
+        l, lab = ax.get_legend_handles_labels()
+        lines += l
+        labels += lab
+    
+    fig.legend(lines, labels, loc='center left', bbox_to_anchor=(0.75, 0.7))
+    #fig.tight_layout()
     plt.savefig('PICS/' + study_type + '/training_loss.pdf', transparent=False)
 
     plt.cla()
