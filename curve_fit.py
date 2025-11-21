0303415c5c1ad7e14e6371d47ddadfe66a786aae
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-21 19:07:43 +0100

    reset

--- curve_fit.py
+++ curve_fit.py
@@ -1,57 +1,68 @@
 import torch
-from file_operations import reset_directory
+from file_operations import reset_directory, create_directory
 from study import study_data
 
-def curve_fit():
+def curve_fit(reset):
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
-    reset_directory('./RESULTS/')
-    reset_directory('./PICS/')
+    if reset == 1:
+        reset_directory('./RESULTS/')
+        reset_directory('./PICS/')
+    else:
+        create_directory('./RESULTS/')
+        create_directory('./PICS/')
+        create_directory('./PICS/sine_noise')
+        create_directory('./RESULTS/sine_noise')
+        create_directory('./PICS/trig_noise')
+        create_directory('./RESULTS/trig_noise')
+        create_directory('./PICS/exp_noise')
+        create_directory('./RESULTS/exp_noise')
+        create_directory('./PICS/hyp_noise')
+        create_directory('./RESULTS/hyp_noise')
+        create_directory('./PICS/quad_noise')
+        create_directory('./RESULTS/quad_noise')
+        create_directory('./PICS/exppoly_noise')
+        create_directory('./RESULTS/exppoly_noise')
+        create_directory('./PICS/trig_noise')
+        create_directory('./RESULTS/trig_noise')
     
     ################### SINE NOISE ##########################
-    reset_directory('./PICS/sine_noise')
-    reset_directory('./RESULTS/sine_noise')
+    
     
     y = torch.sin(x) + 0.2 * torch.randn(x.size())
     study_data(x, y, 1000, 0.0005, study_type='sine_noise')
 
     ################### TRIG NOISE ##########################
-    reset_directory('./PICS/trig_noise')
-    reset_directory('./RESULTS/trig_noise')
+    
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
     y = torch.sin(x) + 0.5 * torch.sin(3*x) + 0.2 * torch.randn(x.size())
     study_data(x, y, 5000, 0.001, study_type='trig_noise')
     
     ################### EXP NOISE ##########################
-    reset_directory('./PICS/exp_noise')
-    reset_directory('./RESULTS/exp_noise')
+    
     
     y = torch.exp(-0.5*x) + 0.2 * torch.randn(x.size())
     study_data(x, y, 10000, 0.0001, study_type='exp_noise')
     
     ################### HYP NOISE ##########################
-    reset_directory('./PICS/hyp_noise')
-    reset_directory('./RESULTS/hyp_noise')
+    
     
     y = torch.tanh(x) + 0.2 * torch.randn(x.size())
     study_data(x, y, 1000, 0.001, study_type='hyp_noise')
     
     ################### QUAD NOISE ##########################
-    reset_directory('./PICS/quad_noise')
-    reset_directory('./RESULTS/quad_noise')
+    
 
     y = torch.tensor(x**2) + 0.2 * torch.randn(x.size())
     study_data(x, y, 2000, 0.001, study_type='quad_noise')
 
     ################### EXP-POLY NOISE ##########################
-    reset_directory('./PICS/exppoly_noise')
-    reset_directory('./RESULTS/exppoly_noise')
+    
     x = torch.linspace(0, 10, 200).unsqueeze(1)
     y = torch.tensor(x**3) / (torch.exp(x) - 1 + 1E-6) + 0.2 * torch.randn(x.size())
     study_data(x, y, 2000, 0.001, study_type='exppoly_noise')
     
     ################### HYP NOISE ##########################
-    reset_directory('./PICS/trig_noise')
-    reset_directory('./RESULTS/trig_noise')
+    
     x = torch.linspace(-5, 5, 200).unsqueeze(1)
     y = torch.sin(x) + 0.5 * torch.sin(3*x) + 0.2 * torch.randn(x.size())
     study_data(x, y, 5000, 0.001, study_type='trig_noise')
