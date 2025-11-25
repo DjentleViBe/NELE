f6133c4ed29ce891c577814d1cce80963a5559cf
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-25 10:39:11 +0100

    validation

--- MNIST/validation.py
+++ MNIST/validation.py
@@ -6,8 +6,10 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from models.model_lelu import LELU
 from models.model_nele import NELE
+import config as cfg
+import numpy as np
 
-def mnist_validation(epochs, device, activation_type='default'):
+def mnist_validation(epochs, device, noise_level = cfg.noise_level, activation_type='default'):
     if '=' in activation_type:
         base, param = activation_type.split('=')
         param = float(param)  # convert parameter to float if needed
@@ -29,13 +31,13 @@ def mnist_validation(epochs, device, activation_type='default'):
     elif base == 'silu' :
         activation = nn.SiLU()
     elif base == 'softplus' :
-        activation = nn.Softmax()
+        activation = nn.Softplus()
     elif base == 'tanh':
         activation = nn.Tanh()
     elif base == 'lelu' :
         activation = LELU()
     elif base == 'nele' :
-        activation = NELE(1, 3, 2)
+        activation = NELE(cfg.batch_size, 3, 2)
     else:
         raise ValueError("Invalid activation type")
     # Load MNIST
@@ -45,21 +47,29 @@ def mnist_validation(epochs, device, activation_type='default'):
     ])
     test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
     model = DeepFCNet(input_size, hidden_size, num_hidden_layers, num_classes, activation, activation_type)
-    checkpoint = torch.load('RESULTS/MNIST/' + activation_type + '/' + activation_type + '_' + str(epochs - 1) + '.pth',
+    test_collect = []
+    for i in range(1, 8):
+        checkpoint = torch.load('RESULTS/MNIST/' + activation_type + '=' + str(i) + '/' + activation_type + '=' + str(i) + '_' + str(epochs - 1) + '.pth',
                             map_location=device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    epoch = checkpoint['epoch']
-    epoch_loss = checkpoint['epoch_loss']
-    model.eval()
-    correct = 0
-    total = 0
+        model.load_state_dict(checkpoint['model_state_dict'])
+        epoch = checkpoint['epoch']
+        epoch_loss = checkpoint['epoch_loss']
+        model.eval()
+        correct = 0
+        total = 0
 
-    with torch.no_grad():
-        for data, target in test_loader:
-            outputs = model(data)
-            _, predicted = torch.max(outputs.data, 1)
-            total += target.size(0)
-            correct += (predicted == target).sum().item()
-    print(f'{activation_type}, Test Accuracy: {100 * correct / total:.2f}%, Training loss : {round(epoch_loss, 4)}')
+        with torch.no_grad():
+            for data, target in test_loader:
+                noise = torch.empty_like(data).uniform_(-noise_level, noise_level)
+                x_noisy = data + noise
+                outputs = model(x_noisy)
+                _, predicted = torch.max(outputs.data, 1)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+        test_acc = 100 * correct / total
+        test_collect.append(test_acc)
+    test_collect = np.asarray(test_collect)
+    test_acc_med = np.median(test_collect)
+    print(f'{activation_type}, Test Accuracy: {test_acc_med:.2f}%')
+    return test_acc_med
