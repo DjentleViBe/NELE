4037d618684bd2f0a41876daa43834d37e3ba381
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-11-29 21:37:19 +0100

    divisiaon

--- CIFAR10/study.py
+++ CIFAR10/study.py
@@ -151,7 +151,7 @@ def cifar10_data(epochs, learn_rate, device, exec, activation_type='default'):
                     _, predicted = outputs.max(1)
                     total_val += y.size(0)
                     correct_val += (predicted == y).sum().item()
-            val_acc = correct_val / total_val
+            val_acc = correct_val / max(total_val, 1.0)
         val_collect.append(val_acc)
         if epoch % 5 == 0:
             model.eval()
