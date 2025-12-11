65448a062cdea62cd8c7f0a0b7ec25a8293a8937
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-12-11 17:34:17 +0100

    recreation plots

--- recreation/mish_recreation.py
+++ recreation/mish_recreation.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # ---------------------------
 # NURBS generator (cubic, 4 ctrl pts)
 # ---------------------------
-def nurbs_gen(control_points, weights, t):
+def nurbs_gen_mish(control_points, weights, t):
     cp = control_points.reshape(4, 2)
     w = weights
 
@@ -45,12 +45,12 @@ t = (x_target - x_target.min()) / (x_target.max() - x_target.min())
 # ---------------------------
 # Optimization objective
 # ---------------------------
-def loss(params):
+def loss_mish(params):
     # params = 4 ctrl points * 2 coords + 4 weights = 12 values
     ctrl = params[:8].reshape(4, 2)
     w = params[8:]
 
-    curve = nurbs_gen(ctrl, w, t)
+    curve = nurbs_gen_mish(ctrl, w, t)
 
     # match x and y separately
     x_cur, y_cur = curve[:, 0], curve[:, 1]
@@ -59,67 +59,3 @@ def loss(params):
     x_desired = x_target
 
     return np.mean((x_cur - x_desired)**2 + (y_cur - y_target)**2)
-
-# ---------------------------
-# Initial guess
-# ---------------------------
-initial_ctrl = np.array([
-    [-4, 0],
-    [-2.2, -0.02],
-    [-0.7, -0.08],
-    [0, 0]
-]).flatten()
-
-initial_weights = np.array([1.0, 0.9, 0.7, 1.0])
-
-init_params = np.concatenate([initial_ctrl, initial_weights])
-
-# ---------------------------
-# Run optimization
-# ---------------------------
-res = minimize(
-    loss,
-    init_params,
-    method='L-BFGS-B',
-    options={'maxiter': 500}
-)
-
-opt_params = res.x
-opt_ctrl = opt_params[:8].reshape(4, 2)
-opt_w = opt_params[8:]
-
-np.set_printoptions(precision=4, suppress=True)
-print("Optimized Control Points:\n", opt_ctrl)
-print("Optimized Weights:\n", opt_w)
-
-opt_ctrl_mnist = np.array([
-    [-1, 0],
-    [-0.1, -0.1],
-    [-1/2**0.5, -1/2**0.5],
-    [0, 0]
-])
-opt_w_mnist = np.array([1.0, 1.0, 1.0, 1.0])
-linear = np.array([[0, 0], [4, 4]])
-linear_2 = np.array([[-1, 0], [-4, 0]])
-# ---------------------------
-# Plot results
-# ---------------------------
-curve = nurbs_gen(opt_ctrl, opt_w, t)
-curve_mnist = nurbs_gen(opt_ctrl_mnist, opt_w_mnist, t)
-plt.figure(figsize=(5, 4))
-plt.plot(np.concatenate([x_target, x_extra]), np.concatenate([y_target, y_extra]), '--', label="mish", color = 'red')
-plt.plot(curve[:, 0], curve[:, 1], label = 'mish approximation', color = 'k', linewidth=0.7)
-#plt.plot(curve_mnist[:, 0], curve_mnist[:, 1], label="NURBS MNIST", color = 'green', linewidth=1.0)
-plt.scatter(opt_ctrl[:, 0], opt_ctrl[:, 1], label="Control Points NURBS", color = 'k', s=3)
-#plt.scatter(opt_ctrl_mnist[:, 0], opt_ctrl_mnist[:, 1], label="Control Points MNIST", color = 'green', marker = '+')
-#plt.plot(linear[:, 0], linear[:, 1], color = 'green', linewidth=1.0)
-#plt.plot(linear_2[:, 0], linear_2[:, 1], color = 'green', linewidth=1.0)
-
-plt.legend()
-plt.grid(True, linewidth = 0.2)
-#plt.title("Optimized Cubic NURBS Approximation of Gmish")
-plt.xlabel("x")
-plt.ylabel("f(x)")
-plt.tight_layout()
-plt.axis('equal')
-plt.savefig('./PICS/Approx_mish.pdf')
---
+++ recreation_full.py
@@ -0,0 +1,96 @@
+import numpy as np
+from recreation.elu_recreation import nurbs_gen_elu, elu, loss_elu
+from recreation.silu_recreation import nurbs_gen_silu, silu, loss_silu
+from recreation.lelu_recreation import nurbs_gen_lelu, lelu, loss_lelu
+from recreation.mish_recreation import nurbs_gen_mish, mish, loss_mish
+from recreation.sigmoid_recreation import nurbs_gen_sigmoid, sigmoid, loss_sigmoid
+from recreation.tanh_recreation import nurbs_gen_tanh, tanh, loss_tanh
+from recreation.softplus_recreation import nurbs_gen_softplus, softplus, loss_softplus
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import config as cfg
+
+typearray = cfg.curve_recreation
+type = 'ELU'
+np.set_printoptions(precision=4, suppress=True)
+initial_ctrl = np.array([
+    [-4, 0],
+    [-2.2, -0.02],
+    [-0.7, -0.08],
+    [0, 0]
+]).flatten()
+
+initial_weights = np.array([1.0, 0.9, 0.7, 1.0])
+init_params = np.concatenate([initial_ctrl, initial_weights])
+loss_functions = {
+    'elu': loss_elu,
+    'silu': loss_silu,
+    'lelu': loss_lelu,
+    'mish': loss_mish,
+    'sigmoid': loss_sigmoid,
+    'tanh': loss_tanh,
+    'softplus': loss_softplus,
+    # add more…
+}
+curve_functions = {
+    'elu': nurbs_gen_elu,
+    'silu': nurbs_gen_silu,
+    'lelu': nurbs_gen_lelu,
+    'mish': nurbs_gen_mish,
+    'sigmoid': nurbs_gen_sigmoid,
+    'tanh': nurbs_gen_tanh,
+    'softplus': nurbs_gen_softplus,
+    # add more…
+}
+target_functions = {
+    'elu': elu,
+    'silu': silu,
+    'lelu': lelu,
+    'mish': mish,
+    'sigmoid': sigmoid,
+    'tanh': tanh,
+    'softplus': softplus,
+    # add more…
+}
+for type in typearray:
+    loss_fn = loss_functions[type.lower()]
+    target_fn = target_functions[type.lower()]
+    res = minimize(
+        loss_fn,
+        init_params,
+        method='L-BFGS-B',
+        options={'maxiter': 500}
+    )
+    # target segment
+    x_target = np.linspace(-4, 0, 200)
+    y_target = target_fn(x_target)
+
+    x_extra = np.linspace(0, 4, 200)
+    y_extra = target_fn(x_extra)
+
+    # reparameterize into t ∈ [0, 1]
+    t = (x_target - x_target.min()) / (x_target.max() - x_target.min())
+
+    opt_params = res.x
+    opt_ctrl = opt_params[:8].reshape(4, 2)
+    opt_w = opt_params[8:]
+
+    cur = curve_functions[type.lower()]
+    curve = cur(opt_ctrl, opt_w, t)
+    plt.figure(figsize=(5, 4))
+    plt.plot(np.concatenate([x_target, x_extra]), np.concatenate([y_target, y_extra]), '--', label=r"$\texttt{ELU}$", color = 'red')
+    plt.rcParams['text.usetex'] = True
+    plt.plot(curve[:, 0], curve[:, 1], label = r'$\texttt{ELU}$ approximation', color = 'k', linewidth=0.7)
+    plt.scatter(opt_ctrl[:, 0], opt_ctrl[:, 1], label=r'Control Points $\texttt{NELE}$', color = 'k', s=3)
+    print(f'\\texttt{{{type}}} & [{round(opt_ctrl[0][0], 2)}, {round(opt_ctrl[0][1], 2)}] &\
+    [{round(opt_ctrl[0][0], 2)}, {round(opt_ctrl[0][1], 2)}] &\
+    [{round(opt_ctrl[0][0], 2)}, {round(opt_ctrl[0][1], 2)}] &\
+    [{round(opt_ctrl[0][0], 2)}, {round(opt_ctrl[0][1], 2)}] &\
+    [{round(opt_w[0], 2)} , {round(opt_w[1], 2)} , {round(opt_w[2], 2)} , {round(opt_w[3], 2)}]\\\\')
+    plt.legend()
+    plt.grid(True, linewidth = 0.2)
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.tight_layout()
+    plt.axis('equal')
+    plt.savefig('./PICS/Approx'+ type +'.pdf')
