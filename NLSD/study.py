864aaf491ee839177b076e7e07249430030f4a58
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-12-17 04:44:16 +0100

    pylint nlsd finish

--- NLSD/study.py
+++ NLSD/study.py
@@ -1,22 +1,33 @@
-from plot_pred import plot_pred
-from plot_loss import plot_loss
-from matplotlib import pyplot as plt
-from file_operations import create_directory
-import numpy as np
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-positional-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
+"""
+Non Linear Synthetic Data
+"""
+
+import torch
+from torch import nn
+from torch import optim
 import config as cfg
 from models.model_lelu import LELU
 from models.model_nele import NELE_LUT, NELE, NELE_LUT_PARAM, NELE_LUT_LEARN
-import torch.nn as nn
-import torch
 from csv_operations import csv_write
-import torch.optim as optim
+from file_operations import create_directory
 from NLSD.neuralnet import Net
 
 def nlsd_data(x, y, af, device='cpu', study_type='default'):
+    """
+    Docstring for nlsd_data
+    
+    :param x: X values
+    :param y: Y values
+    :param af: Activation function type
+    :param device: device name
+    :param study_type: Study name
+    """
     loss_collect = []
-    val_collect = []
-    test_collect = []
-    dir = 'RESULTS/NLSD/' + study_type + '/'
+    directory = 'RESULTS/NLSD/' + study_type + '/'
     learning_rate = cfg.learning_rate
     if '=' in af:
         base, param = af.split('=')
@@ -24,37 +35,22 @@ def nlsd_data(x, y, af, device='cpu', study_type='default'):
     else:
         base = af
         param = None
-    if base == 'relu':
-        activation = nn.ReLU()
-        learning_rate = cfg.learning_rate_array[6]
-    elif base == 'elu':
-        activation = nn.ELU(alpha=1.0)
-        learning_rate = cfg.learning_rate_array[3]
-    elif base == 'leaky_relu' :
-        activation = nn.LeakyReLU(negative_slope=0.1)
-        learning_rate = cfg.learning_rate_array[7]
-    elif base == 'gelu' :
-        activation = nn.GELU()
-        learning_rate = cfg.learning_rate_array[5]
-    elif base == 'mish' :
-        activation = nn.Mish()
-        learning_rate = cfg.learning_rate_array[9]
-    elif base == 'sigmoid' :
-        activation = nn.Sigmoid()
-        learning_rate = cfg.learning_rate_array[1]
-    elif base == 'silu' :
-        activation = nn.SiLU()
-        learning_rate = cfg.learning_rate_array[4]
-    elif base == 'softplus' :
-        activation = nn.Softplus()
-        learning_rate = cfg.learning_rate_array[2]
-    elif base == 'tanh':
-        activation = nn.Tanh()
-        learning_rate = cfg.learning_rate_array[0]
-    elif base == 'lelu' :
-        activation = LELU()
-        learning_rate = cfg.learning_rate_array[8]
-    elif base == 'nele' :
+
+    dispatch = {
+        'relu': nn.ReLU(),
+        'elu' : nn.ELU(alpha=1.0),
+        'leaky_relu' : nn.LeakyReLU(negative_slope=0.1),
+        'gelu' : nn.GELU(),
+        'mish' : nn.Mish(),
+        'sigmoid' : nn.Sigmoid(),
+        'silu' : nn.SiLU(),
+        'softplus' : nn.Softplus(),
+        'tanh': nn.Tanh(),
+        'lelu' : LELU(),
+    }
+    if base != "nele":
+        activation = dispatch[base]
+    elif base == "nele":
         if cfg.TYPE == 0:
             activation = NELE()
             learning_rate = cfg.learning_rate_array[10]
@@ -64,12 +60,13 @@ def nlsd_data(x, y, af, device='cpu', study_type='default'):
         elif cfg.TYPE == 1:
             activation = NELE_LUT_PARAM(device=device, num_points=200)
             learning_rate = cfg.learning_rate_array[10]
-        else: 
+        else:
             activation = NELE_LUT()
             learning_rate = cfg.learning_rate_array[10]
     else:
         raise ValueError("Invalid activation type")
-    dir = dir + '/' + af
+
+    directory = directory + '/' + af
     create_directory('RESULTS/NLSD/' + study_type + '/' + base + '/')
     create_directory('PICS/NLSD/' + study_type + '/' + base + '/')
 
@@ -97,9 +94,10 @@ def nlsd_data(x, y, af, device='cpu', study_type='default'):
     loss_collect = torch.tensor(loss_collect)
 
     # Write to CSV
-    pred_file = dir + '/predictions_' + af + '.csv'
-    loss_file = dir + '/loss_history_' + af + '.csv'
+    pred_file = directory + '/predictions_' + af + '.csv'
+    loss_file = directory + '/loss_history_' + af + '.csv'
     csv_write(pred_file, x, predicted, 'x' , 'y_pred', 'y_actual', y)
-    csv_write(loss_file, torch.linspace(1, cfg.epochs, cfg.epochs), loss_collect,  'epoch', 'loss', '', torch.linspace(1, cfg.epochs, cfg.epochs))
+    csv_write(loss_file, torch.linspace(1, cfg.epochs, cfg.epochs), \
+              loss_collect,  'epoch', 'loss', '', torch.linspace(1, cfg.epochs, cfg.epochs))
     sigma_est = torch.std(y - predicted)
-    return loss.item(), sigma_est.item() 
+    return loss.item(), sigma_est.item()
