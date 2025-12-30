6a15587625f4537d30a4df000e6b648ecd9d25cf
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2025-12-30 01:21:31 +0100

    variation band

--- recreation/gelu_recreation.py
+++ recreation/gelu_recreation.py
@@ -5,11 +5,12 @@
 Recreating GELU with NELE
 """
 import numpy as np
+from itertools import product
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from recreation.nurbs_gen import nurbs_gen
 from recreation.gen_loss import loss_af
-
+import config as cfg
 # ---------------------------
 # GELU
 # ---------------------------
@@ -71,30 +72,53 @@ def gelu_special():
     opt_w_mnist = np.array([1.0, 1.0, 1.0, 1.0])
     linear = np.array([[0, 0], [4, 4]])
     linear_2 = np.array([[-8, 0], [-4, 0]])
+    curves = []
+    for cp0x, cp1x, cp1y, length, w_0, w_1, w_2, w_3 in product(
+        cfg.range_cp0_x,
+        cfg.range_cp1_x,
+        cfg.range_cp1_y,
+        cfg.range_l,
+        cfg.range_w0,
+        cfg.range_w1,
+        cfg.range_w2,
+        cfg.range_w3,
+    ):
+        cp0 = [cp0x, 0.0]
+        cp1 = [cp1x, cp1y]
+        cp2 = [length/1.4142, length/1.41422]
+        cp3 = [0.0, 0.0]
+        opt_ctrl_range = np.array([cp0, cp1, cp2, cp3])
+        opt_w_range = np.array([w_0, w_1, w_2, w_3])
+        curves.append(nurbs_gen(opt_ctrl_range, opt_w_range, t_val))
+    curves = np.stack(curves)
+    mean_curve = curves.mean(axis=0)
+    lower = curves.min(axis=0)
+    upper = curves.max(axis=0)
     # ---------------------------
     # Plot results
     # ---------------------------
+    x_mean = mean_curve[:, 0]
     curve_gen = nurbs_gen(opt_ctrl, opt_w, t_val)
-    curve_mnist = nurbs_gen(opt_ctrl_mnist, opt_w_mnist, t_val)
+    #curve_mnist = nurbs_gen(opt_ctrl_mnist, opt_w_mnist, t_val)
     plt.figure(figsize=(5, 4))
     plt.rcParams['text.usetex'] = True
     plt.plot(np.concatenate([x_target, x_extra]), np.concatenate([y_target, y_extra]), \
             '--', label=r'$\texttt{GELU}$', color = 'red')
     plt.plot(curve_gen[:, 0], curve_gen[:, 1], label = r'$\texttt{GELU}$ approximation', \
             color = 'k', linewidth=0.7)
-    plt.plot(curve_mnist[:, 0], curve_mnist[:, 1], label=r'$\texttt{NELE}$ experiment', \
-            color = 'green', linewidth=1.0)
+    plt.plot(x_mean, mean_curve[:, 1], label=r'$\texttt{NELE}$ experiment mean', \
+            color = 'blue', linewidth=0.7)
+    plt.fill_between(
+    x_mean,
+    lower[:, 1],
+    upper[:, 1],
+    alpha=0.3,
+    label="Hyper-parameter variation band"
+)
     plt.scatter(opt_ctrl[:, 0], opt_ctrl[:, 1], label=r'Control Points $\texttt{GELU}$ approx', \
                 color = 'k', s=3)
-    plt.scatter(opt_ctrl_mnist[:, 0], opt_ctrl_mnist[:, 1], label=r"Control Points $\texttt{NELE}$ exp", \
-                color = 'green', marker = '+')
-    # Label each point p0, p1, p2, p3
-    for i, (x_val, y) in enumerate(opt_ctrl_mnist):
-        plt.annotate(f"p{i}", (x_val, y), textcoords="offset points", \
-                    xytext=(5, -4), fontsize=6, color='green')
-    plt.plot(linear[:, 0], linear[:, 1], color = 'green', linewidth=1.0)
-    plt.plot(linear_2[:, 0], linear_2[:, 1], color = 'green', linewidth=1.0)
-
+    plt.plot(linear[:, 0], linear[:, 1], color = 'blue', linewidth=0.7)
+    plt.plot(linear_2[:, 0], linear_2[:, 1], color = 'blue', linewidth=0.7)
     plt.legend()
     plt.grid(True, linewidth = 0.2)
     #plt.title("Optimized Cubic NURBS Approximation of GELU")
